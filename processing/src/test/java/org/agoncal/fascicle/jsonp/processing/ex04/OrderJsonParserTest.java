086c6651efc6526713191d63b8048c4bd23435fd
Antonio Goncalves <antonio.goncalves@gmail.com>
2017-09-13 17:22:29 +0200

    Json reader

---
+++ processing/src/test/java/org/agoncal/fascicle/jsonp/processing/ex04/OrderJsonParserTest.java
@@ -0,0 +1,27 @@
+package org.agoncal.fascicle.jsonp.processing.ex04;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public class OrderJsonParserTest {
+
+  // ======================================
+  // =              Unit tests            =
+  // ======================================
+
+  @Test
+  public void shouldBuildPurchaseOrder() throws FileNotFoundException {
+    // TODO not a great test
+    assertNull(new OrderJsonParser().parsePurchaseOrderAndReturnEmail());
+
+  }
+}
