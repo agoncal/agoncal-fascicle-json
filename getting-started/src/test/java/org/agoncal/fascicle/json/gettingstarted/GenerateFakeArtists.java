101ada1972070adeff247ec6d7a6894ad67ef7be
Antonio Goncalves <antonio.goncalves@gmail.com>
2019-11-28 19:02:35 -0800

    Getting started

--- getting-started/src/test/java/org/agoncal/fascicle/json/gettingstarted/GenerateFakeArtists.java
+++ getting-started/src/test/java/org/agoncal/fascicle/json/gettingstarted/GenerateFakeArtists.java
@@ -3,8 +3,7 @@ package org.agoncal.fascicle.json.gettingstarted;
 import com.github.javafaker.Faker;
 
 import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonWriter;
+import javax.json.stream.JsonGenerator;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -12,17 +11,30 @@ public class GenerateFakeArtists {
 
   public static void main(String[] args) throws IOException {
     Faker faker = new Faker();
-    JsonWriter writer = Json.createWriter(new FileWriter("artists.json"));
-    for (int i = 0; i < 10_000; i++) {
-      JsonObject json = Json.createObjectBuilder().
-        add("first-name", faker.name().firstName()).
-        add("last-name", faker.name().lastName()).
-        add("email", faker.internet().emailAddress()).
-        add("bio", faker.lorem().characters()).
-        add("date-birth", faker.date().birthday().toString()).
-        build();
-      writer.write(json);
+    FileWriter writer = new FileWriter("artists.json");
+    JsonGenerator generator = Json.createGenerator(writer);
+    generator.writeStartArray();
+
+    for (int i = 0; i < 1_000; i++) {
+//      JsonObject json = Json.createObjectBuilder().
+//        add("first-name", faker.name().firstName()).
+//        add("last-name", faker.name().lastName()).
+//        add("email", faker.internet().emailAddress()).
+//        add("bio", faker.lorem().characters()).
+//        add("date-birth", faker.date().birthday().toString()).
+//        build();
+
+      generator.writeStartObject();
+      generator.write("first-name", faker.name().firstName());
+      generator.write("last-name", faker.name().lastName());
+      generator.write("email", faker.internet().emailAddress());
+      generator.write("bio", faker.lorem().paragraph());
+      generator.write("date-birth", faker.date().birthday().toString());
+      generator.writeEnd();
     }
+    generator.writeEnd();
+    generator.close();
+
     //writer.close();
   }
 }
